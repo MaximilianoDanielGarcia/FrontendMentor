0175b2e1ad1a591f0ac451f03032d85f0c8db28b
Maximiliano Daniel García <maxi.garcia11@hotmail.com>
2023-10-01 17:16:41 -0300

    Fix vite config

--- time-tracking-dashboard/vite.config.js
+++ time-tracking-dashboard/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/time-tracking-dashboard/"
+  base: "https://github.com/MaximilianoDanielGarcia/FrontendMentor/time-tracking-dashboard/"
 })
