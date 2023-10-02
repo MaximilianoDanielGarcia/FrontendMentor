ee3c64a1192eaa95f2a0b1fd8e71970ac4daaf79
Maximiliano Daniel García <maxi.garcia11@hotmail.com>
2023-10-02 17:09:51 -0300

    changing base url into vite config

--- time-tracking-dashboard/vite.config.js
+++ time-tracking-dashboard/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/MaximilianoDanielGarcia/FrontendMentor/time-tracking-dashboard/"
+  base: "https://master--bucolic-lokum-e59bc1.netlify.app/"
 })
